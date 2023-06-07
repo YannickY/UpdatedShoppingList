2939de17438c2c7b26d324e5eac0e3017e1b14e1
YannickY <96672975+YannickY@users.noreply.github.com>
2023-06-07 18:15:25 -0400

    Add files via upload

---
+++ ShoppingList.js
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react';
+import "./React.css"
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function onRemoveItem(itemToRemove) {
+    
+    const newItems = items.filter((item) => {
+      
+      return item !== itemToRemove;
+    });
+    setItems(newItems);
+    console.log(newItems);
+  }
+
+  function onSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const input = form.item;
+    const newItems = [...items, input.value];
+    setItems(newItems);
+    form.reset();
+     
+  }
+
+  return (
+    <>
+      <h1>Project: Shopping List</h1>
+      <div className="shopping-list">
+        <h2>Items To Buy</h2>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="item"
+            placeholder="Add a new item"
+            required
+          /> 
+        
+          <button>Add</button>
+        </form>
+        <ul>
+          {items.map((item, index) => (
+            <li>{item}<button onClick={() => onRemoveItem(item)}>ADD</button></li> 
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+function Item({ item, onRemoveItem }) {
+  return (
+    <li>
+      {item}
+      <button className="delete" onClick={() => onRemoveItem(item)}>
+        x
+      </button>
+    </li>
+  );
+}
